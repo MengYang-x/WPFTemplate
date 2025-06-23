faec16ea32843d703d3b388a0ff576e689b15533
mengyang <mengyang@cvte.com>
2025-06-23 08:56:28 +0800

    first commit

---
+++ NavigationTemplate/NavigationTemplate/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace NavigationTemplate;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
